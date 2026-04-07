3687e37afe025fbcb2b729a53fc2b18fb02470d4
xaxxoo <bnahmad83@gmail.com>
2026-04-07 12:32:06 +0100

    Turbopack added to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // This tells Turbopack to only look inside your current project folder
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
